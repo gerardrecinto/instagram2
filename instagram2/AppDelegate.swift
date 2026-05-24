b5eddf7e453d8894d58e2db8eec637e73bc5c95e
Gerard Louis Andres Recinto <grecinto@ucsd.edu>
2026-05-24 15:56:38 -0700

    fix: remove broken Parse SDK references from AppDelegate, use LocalInstagramService

--- instagram2/AppDelegate.swift
+++ instagram2/AppDelegate.swift
@@ -7,47 +7,16 @@
 //
 
 import UIKit
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
+    var window: UIWindow?
 
-
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-     -> Void in
-        configuration.applicationId = "Instagram"
-        configuration.clientKey = "askdj;alcds;lcskak;lsadcjsad;klcj"  // set to nil assuming you have not set clientKey
-        configuration.server = "https://fast-citadel-91087.herokuapp.com/parse"
-      }))
-    if PFUser.current() != nil {
-      // if there is a logged in user then load the home view controller
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      PFUser.logOutInBackground()
-      let vc = storyboard.instantiateInitialViewController()
-      window?.rootViewController = vc
-    } else {
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      let vc = storyboard.instantiateInitialViewController()
-      window?.rootViewController = vc
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+        window?.rootViewController = vc
+        return true
     }
-    // Code to initialize Parse
-    // (See above section 'Parse `initializeWithConfiguration` vs `setApplicationId`', if you have not already set it up)
-
-    // check if user is logged in.
-   /* if PFUser.current() != nil {
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      // view controller currently being set in Storyboard as default will be overridden
-      window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "FeedViewController")
-    }*/
-
-    return true
-  }
-
-
-
-
-
-
 }
-
